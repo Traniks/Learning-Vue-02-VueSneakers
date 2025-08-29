c26767444b31fed8e35dea80bad7e1f53119e371
Traniks <andert2017@gmail.com>
2025-08-29 13:53:07 +0300

    feat: add ProductList and ProductCard components with search and filter (no API)

---
+++ src/components/ProductList.vue
@@ -0,0 +1,32 @@
+<script setup>
+import ProductCard from './ProductCard.vue';
+</script>
+
+<template>
+  <section class="px-[60px]">
+    <div class="flex justify-between items-center">
+      <h2 class="text-[32px] font-bold">Все кросовки</h2>
+
+      <div class="flex justify-between items-center gap-[20px]">
+        <select name="category" id="select-category" class="px-3 py-2 rounded-md border border-gray-300 outline-none">
+          <option value="all">Все</option>
+          <option value="men">Мужские</option>
+          <option value="women">Женские</option>
+        </select>
+
+        <div class="relative">
+          <img src="@/assets/icons/search.svg" alt="search" class="absolute left-4 top-3">
+          <input type="text" placeholder="Поиск..."
+            class="pl-11 py-2 pr-2 border border-gray-300 focus:border-gray-400 outline-none rounded-md" />
+        </div>
+      </div>
+    </div>
+
+    <div class="grid grid-cols-4 gap-[40px] py-[40px]">
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </div>
+  </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,9 @@
 <script setup>
 import SimpleSlider from '@/components/SimpleSlider.vue'
+import ProductList from '@/components/ProductList.vue'
 </script>
 
 <template>
   <SimpleSlider />
-  <h1>This is a home page</h1>
+  <ProductList />
 </template>
