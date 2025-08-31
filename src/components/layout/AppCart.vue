acb56dac7716cabe8a964fde937295628749c4bd
Traniks <andert2017@gmail.com>
2025-08-31 22:16:27 +0300

    feat: add cart items display and improve cart UI (no API)

--- src/components/layout/AppCart.vue
+++ src/components/layout/AppCart.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import BaseButton from '@/components/ui/BaseButton.vue'
+import CartItem from '@/components/features/CartItem.vue'
 import { useAppStore } from '@/stores/app'
 
 import EmptyImg from '@/assets/img/package-icon.png'
@@ -14,11 +15,20 @@ const appStore = useAppStore()
       @click="appStore.closeCart"></div>
 
     <div class="fixed top-0 right-0 z-6 bg-white h-full w-[385px] pt-[32px] pb-[30px] px-[30px]">
-      <h2 class="text-[28px] font-bold">Корзина</h2>
+      <div class="flex items-center gap-[15px]">
+        <svg class="rotate-180 stone-950 cursor-pointer opacity-50 hover:opacity-100 transition duration-300"
+          @click="appStore.closeCart" width="16" height="14" viewBox="0 0 16 14" fill="none"
+          xmlns="http://www.w3.org/2000/svg">
+          <path d="M1 7H14.7143" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M8.71436 1L14.7144 7L8.71436 13" stroke="black" stroke-width="2" stroke-linecap="round"
+            stroke-linejoin="round" />
+        </svg>
 
-      <div class="py-[30px] flex flex-col items-center justify-center h-full">
+        <h2 class="text-[28px] font-bold">Корзина</h2>
+      </div>
 
-        <template v-if="true">
+      <template v-if="appStore.cartState === 'empty'">
+        <div class="py-[30px] flex flex-col items-center h-full justify-center">
           <img :src="EmptyImg" alt="package" width="120">
           <h3 class="mt-[20px] font-semibold text-[22px] text-center">Корзина пустая</h3>
           <p class="mt-[10px] text-[16px] opacity-40 text-center">Добавьте хотя бы одну пару кроссовок, чтобы сделать
@@ -29,25 +39,41 @@ const appStore = useAppStore()
                 class="absolute translate-y-[-50%] top-[50%] rotate-180 left-[60px]" />
             </template>
           </BaseButton>
-        </template>
+        </div>
+      </template>
 
-        <template>
-          <div class="py-[30px] flex flex-col items-center justify-between">
-            <p>Товары, которые вы добавили в корзину:</p>
-            <div>
-              <p></p>
-              <p></p>
-              <BaseButton class="w-full">Оформить заказ
-                <template #icon>
-                  <img src="@/assets/icons/arrow-next.svg" alt="arrow"
-                    class="absolute translate-y-[-50%] top-[50%] left-[60px]" />
-                </template>
-              </BaseButton>
-            </div>
+      <template v-if="appStore.cartState === 'filled'">
+        <div class="py-[30px] flex flex-col h-full justify-between">
+
+          <div class="flex flex-col items-center gap-[20px]">
+            <CartItem v-for="item in 2" :key="item.id" :id="item.id" :title="item.title" :price="item.price" />
           </div>
-        </template>
 
-        <template>
+          <div>
+            <div class="flex gap-2">
+              <span class="text-[18px]">Итого:</span>
+              <div class="flex-1 border-b border-dashed"></div>
+              <b class="text-[16px] font-bold">10 000 ₽</b>
+            </div>
+
+            <div class="flex gap-2 mt-20px">
+              <span class="text-[18px]">Налог 5%:</span>
+              <div class="flex-1 border-b border-dashed"></div>
+              <b class="text-[16px] font-bold">1000 ₽</b>
+            </div>
+
+            <BaseButton class="w-full mt-[24px]">Оформить заказ
+              <template #icon>
+                <img src="@/assets/icons/arrow-next.svg" alt="arrow"
+                  class="absolute translate-y-[-50%] top-[50%] right-[60px]" />
+              </template>
+            </BaseButton>
+          </div>
+        </div>
+      </template>
+
+      <template v-if="appStore.cartState === 'ordered'">
+        <div class="py-[30px] flex flex-col items-center h-full justify-center">
           <img :src="OrderImg" alt="order success" width="83">
           <h3 class="success mt-[30px] font-semibold text-[22px] text-center">Заказ оформлен!</h3>
           <p class="mt-[10px] text-[16px] opacity-40 text-center">Ваш заказ #18 скоро будет передан курьерской
@@ -58,9 +84,9 @@ const appStore = useAppStore()
                 class="absolute translate-y-[-50%] top-[50%] rotate-180 left-[60px]" />
             </template>
           </BaseButton>
-        </template>
+        </div>
+      </template>
 
-      </div>
     </div>
   </div>
 </template>
